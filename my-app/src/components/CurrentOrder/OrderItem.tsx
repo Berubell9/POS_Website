9529e6c74c14244af61406d8be251667eb115982
Klinglunlun <79560837+Berubell9@users.noreply.github.com>
2026-03-10 04:08:46 +0700

    Add comment

--- my-app/src/components/CurrentOrder/OrderItem.tsx
+++ my-app/src/components/CurrentOrder/OrderItem.tsx
@@ -5,10 +5,14 @@ export default function OrderItem() {
             <div className="flex items-center gap-3">
                 <img src="test.jpg" className="w-16 h-16 object-cover rounded-md" />
                 <div className="space-y-2">
+                    {/* ชื่อเมนู */}
                     <p className="font-semibold">ชื่อเมนู</p>
                     <div className="flex items-center gap-3">
+                        {/* ปุ่มลบจำนวน */}
                         <button className="w-6 h-6 text-sm text-gray-500 rounded-full border border-gray-200 flex items-center justify-center">-</button>
+                        {/* จำนวน */}
                         <p className="text-sm">1</p>
+                        {/* ปุ่มเพิ่มจำนวน */}
                         <button className="w-6 h-6 text-sm text-gray-500 rounded-full border border-gray-200 flex items-center justify-center">+</button>
                     </div>
                 </div>
@@ -16,7 +20,9 @@ export default function OrderItem() {
 
             {/* ขวา */}
             <div className="flex flex-col items-end gap-2">
+                {/* ปุ่มลบรายการ */}
                 <button className="h-6 w-6 text-gray-500">✕</button>
+                {/* ราคา */}
                 <p className="text-pink-400 font-bold"><span>฿</span>55</p>
             </div>
         </div>
